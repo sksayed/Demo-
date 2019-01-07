d2ec7ababbee6c61dc001edd488bebcb4a276334
sayed bin rahman <sayedrahman015@gmail.com>
2019-01-07 18:48:35 +0600

    mixed reset kore abar fire aslam

---
+++ basic math.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std ;
+
+int add (int a , int b )
+{
+    pass ;
+}
+int sub (int a , int b )
+{
+    pass ;
+}
+int mul (int a , int b )
+{
+    pass ;
+}
+int div (int a , int b )
+{
+    pass ;
+}
+
+
+
+int main ()
+{
+
+
+
+}
